473b2c42743755adc3d23566155efbbbb9835be9
bdegroot@snemail.com <bdegroot@snemail.com>
2022-02-19 19:06:06 +0000

    commit

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,9 @@
+select
+        ID as payment_id,
+        ORDERID as order_id,
+        PAYMENTMETHOD as payment_method,
+        STATUS,
+        AMOUNT/100 as amount,
+        CREATED as created_at
+
+from RAW.STRIPE.PAYMENT
